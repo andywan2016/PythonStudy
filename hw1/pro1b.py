c30bd21b899386872e6ae80cb177e78235aa548f
zw2327 <zw2327@dsc400mac05.cul.columbia.edu>
2016-06-16 13:04:54 -0400

    1c in process

--- hw1/pro1b.py
+++ hw1/pro1b.py
@@ -1,22 +1,28 @@
+import pro7
+
 def shortlong(tv0,tv1):
-  l=[]
   if len(tv0)<len(tv1):
-    l=l.append(tv0)
-    l=l.append(len(tv0))
-    l=l.append(tv1)
-    l=l.append(len(tv1))
-
+    l=[tv0,len(tv0),tv1,len(tv1)]
   else:
-    l=l.append(tv1)
-    l=l.append(len(tv1))
-    l=l.append(tv0)
-    l=l.append(len(tv0))
-
+    l=[tv1,len(tv1),tv0,len(tv0)]
   return l
+
+#problem 1c
+def odp(tv0,tv1,offset):
+	info=shortlong(tv0,tv1)
+	if offset>(info[3]-info[1]):
+		print "offset number too large"
+		return None
+	else:
+		return dp(info[0],info[2][offset:])
+
 
 def main():
   a=[1,2,3]
   b=[4,5,6,7,8,9]
   print shortlong(a,b)
+  c1=[odp(a,b,0),odp(a,b,1),odp(a,b,2)]
+  print c1
+
 
 main()
