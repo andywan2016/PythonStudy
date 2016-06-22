8abe264e49a1c591a69b6081feebfc97c43fd28a
andywan2016 <zw2327@columbia.edu>
2016-06-22 05:48:43 -0400

    still have questions on 1 d

--- hw1/pro1b.py
+++ hw1/pro1b.py
@@ -1,4 +1,4 @@
-import pro7
+from pro7 import dp
 
 def shortlong(tv0,tv1):
   if len(tv0)<len(tv1):
@@ -17,12 +17,36 @@ def odp(tv0,tv1,offset):
 		return dp(info[0],info[2][offset:])
 
 
+#problem 1d
+def dpd(tv0,tv1,pad):
+  global s,l
+  s,sl,l,ll=shortlong(tv0,tv1)
+  print type(s)
+  print s
+  print hex(id(s))
+  dl=ll-sl
+  s=s[:]
+  print s
+  print hex(id(s))
+  print type(s) 
+  s.extend(dl*[pad])
+ 
+  print hex(id(s))
+  return dp(s,l)
+
+
+
+
 def main():
   a=[1,2,3]
   b=[4,5,6,7,8,9]
-  print shortlong(a,b)
-  c1=[odp(a,b,0),odp(a,b,1),odp(a,b,2)]
-  print c1
+  #print shortlong(a,b)
+ # c1=[odp(a,b,0),odp(a,b,1),odp(a,b,2)]
+  #print c1  
+ # d1=[dpd(a,b,0),dpd(a,b,1),dpd(a,b,2)]
+ # print d1
+ # print(dpd(a,b,1))
+  print(dpd(a,b,2))  
 
 
 main()
