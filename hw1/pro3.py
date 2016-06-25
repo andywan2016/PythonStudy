82b6f62af544cf43bdf0e155df65c8655396824e
andywan2016 <zw2327@columbia.edu>
2016-06-25 00:09:51 -0400

    almost

--- hw1/pro3.py
+++ hw1/pro3.py
@@ -6,17 +6,28 @@ Created on Thu Jun 23 01:26:35 2016
 """
 
 def partition(l,n,overlap):
-    if type(l) is range:
-        return 0
-    if type(l) is list:
-        return 1
-    else:
-        print("unsupported type")
+    ls=list(l)
+    ls.append(ls[-1]+1)
+    if len(ls)<n:
         return None
+    else:
+        output=[range(ls[0],ls[n])]
+        i=n-overlap
+        while i+n <len(ls):
+            output.append(range(ls[i],ls[i+n]))
+            i+=n-overlap
+        if type(l) is range:
+            return output
+        if type(l) is list:
+            return list(map(list,output))
+        else:
+            print("unsupported type")
+            return None
+
         
 def main():
-    print(partition(range(10),0,1))
-    print(partition(list(range(10)),0,1))
-    print(partition(1,0,1))
+    print(partition(range(10),2,1))
+    print(partition(list(range(10)),2,0))
+   # print(partition(1,0,1))
 
 main()
