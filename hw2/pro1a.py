85edd883af0b4428362f91db6db264f44a7b1a55
andywan2016 <zw2327@columbia.edu>
2016-07-01 03:26:13 -0400

    reform

--- hw2/pro1a.py
+++ hw2/pro1a.py
@@ -1,42 +1,42 @@
 def decimals(n):
     def getpivot(num,pivot):
-            i=0
-                    pivot*=10
-                            while pivot<num:
-                                            pivot*=10
-                                                        #yield(0)
-                                                                    i+=1
-                                                                            return [pivot,i]
-                                                                                #def formlist(poi,ls):
-                                                                                       # for x in range(0,poi[1]):
-                                                                                                   #ls.append(0)
-                                                                                                          # ls.append(poi[0]
-                                                                                                              x=1
-                                                                                                                  while x<n:
-                                                                                                                              x*=10
-                                                                                                                                  
-                                                                                                                                      output1=(x//n)
-                                                                                                                                          yield(output1)
-                                                                                                                                              res=x%n
-                                                                                                                                                  
-                                                                                                                                                      while True:
-                                                                                                                                                                  if res is 0:
-                                                                                                                                                                                  return
-                                                                                                                                                                                          else:
-                                                                                                                                                                                                          x=getpivot(n,res)
-                                                                                                                                                                                                                      for j in range(0,x[1]):
-                                                                                                                                                                                                                                          yield(0)
-                                                                                                                                                                                                                                                      output1=x[0]//n
-                                                                                                                                                                                                                                                                  yield(output1)
-                                                                                                                                                                                                                                                                              res=x[0]%n
-                                                                                                                                                                                                                                                                                      
-                                                                                                                                                                                                                                                                                              
-                                                                                                                                                                                                                                                                                                      
-                                                                                                                                                                                                                                                                                                      def main():
-                                                                                                                                                                                                                                                                                                         #a=decimals(18)
-                                                                                                                                                                                                                                                                                                            #print(list(a))
-                                                                                                                                                                                                                                                                                                               d=decimals(245)
-                                                                                                                                                                                                                                                                                                                  print([next(d) for x in range(10)])
-                                                                                                                                                                                                                                                                                                                      
-                                                                                                                                                                                                                                                                                                                      main()
+        i=0
+        pivot*=10
+        while pivot<num:
+            pivot*=10
+            #yield(0)
+            i+=1
+        return [pivot,i]
+    #def formlist(poi,ls):
+       # for x in range(0,poi[1]):
+            #ls.append(0)
+       # ls.append(poi[0]
+    x=1
+    while x<n:
+        x*=10
+    
+    output1=(x//n)
+    yield(output1)
+    res=x%n
+    
+    while True:
+        if res is 0:
+            return
+        else:
+            x=getpivot(n,res)
+            for j in range(0,x[1]):
+                yield(0)
+            output1=x[0]//n
+            yield(output1)
+            res=x[0]%n
+        
+        
+        
+def main():
+   #a=decimals(18)
+   #print(list(a))
+   d=decimals(245)
+   print([next(d) for x in range(10)])
+    
+main()
 
