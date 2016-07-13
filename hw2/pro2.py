9bdd4126ab04a03e162062f8de274d4b0d59a2f4
andywan2016 <zw2327@columbia.edu>
2016-07-13 00:57:45 -0400

    need test

--- hw2/pro2.py
+++ hw2/pro2.py
@@ -7,9 +7,36 @@ def decimal2(num):
 	i=2
 	while pivot:
 		ls=genlimit(decimals(num),i)
-		lth=len(ls)
-		
+        lth=len(ls)
+#for a finite stirng,just return
+        if lth<i:
+            return ls
+
 		ls_1=ls[0:ls-1]
+        in_pv=ls[-1]
+
+        if in_pv not in ls_1:
+            i+=1
+            continue
+        else:
+            new_len=(lth-ls.index(in_pv)-2)+i
+            expand_ls=genlimit()(decimals(num),new_len)
+            left_ls=expand_ls[ls.index(in_pv):i]
+            right_ls=expand_ls[i:len(expand_ls)]
+
+            if (left_ls==right_ls).all():
+                return expand_ls
+
+            else:
+                i+=1
+                continue
+
+
+
+
+
+
+
 
 
 
