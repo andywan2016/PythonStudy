d651a67d15fdf6400070032f7b422e9dacbe30fa
ZHIBO WAN <andywan@dyn-160-39-138-34.dyn.columbia.edu>
2016-07-25 16:55:27 -0400

    this part work well

--- hw2/pro3c.py
+++ hw2/pro3c.py
@@ -1,3 +1,5 @@
+"""
+
 def powerSet(l):
 	output_list=[]
 	output_list.append([])
@@ -7,9 +9,33 @@ def powerSet(l):
 			tmp=x
 			tmp.append(e)
 			output_list.append(tmp)
+"""
+#this function is used for
+def listExtend(ls,element):
+	ls1,ls2=[],[]
+	ls1.extend(ls)
+	ls2.extend(ls)
+	#ls1.extend(" ")
+	ls2.append(element)
+	return [ls1,ls2]
+
+
+def powerSet(l):
+	output=[[]]
+	for x in l:
+		tmpList=[]
+		for y in output:
+			tmpList.extend(listExtend(y,x))
+		output=[]
+		output.extend(tmpList)
+
+	return output
 
 
 def main():
+#test for listExtend	
+#	print(listExtend(["A1","B1","C1"],"D1"))
+	
 	ls=powerSet(['avery','math','butler'])
 	print(ls)
 
