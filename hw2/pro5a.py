88df6b4140bcd88f95cf3c73592231e5613cdf12
ZHIBO WAN <andywan@ZHIBOs-MacBook-Air.local>
2016-07-25 21:34:54 -0400

    this part done

--- hw2/pro5a.py
+++ hw2/pro5a.py
@@ -1,12 +1,26 @@
 def countBases(dna):
-	return None
+	board=[0,0,0,0]
+	for x in dna:
+		if x=='A':
+			board[0]+=1
+		elif x=='C':
+			board[1]+=1
+		elif x=='G':
+			board[2]+=1
+		elif x=='T':
+			board[3]+=1
+		else:
+			print("invalid dna kye")
+			return None
+
+	return board
 
 
 def main():
 	bases='ACGT'
 	dna='CATCGATATCTCTGAGTGCAC'
 	print(countBases('AC'))
-	print(countBasese(dna))
+	print(countBases(dna))
 
 
 main()
