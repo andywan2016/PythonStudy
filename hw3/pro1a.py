63731356b6e184dd826dc1730f3e48ca1e85a89a
ZHIBO WAN <andywan@dyn-160-39-138-24.dyn.columbia.edu>
2016-08-05 14:40:53 -0400

    code works,but result shit,looking

--- hw3/pro1a.py
+++ hw3/pro1a.py
@@ -1,6 +1,13 @@
 import string
 from pro1 import swapaz,bad,bad2
 
+def ifSame(strr):
+	for x in range(len(strr)-1):
+		if strr[x] in strr[x+1:]:
+			return False
+	return True
+
+
 
 def goodperm(cl):
 	al=string.ascii_lowercase
@@ -12,15 +19,16 @@ def goodperm(cl):
 		coded=cl.encode(e)
 		if e!=coded:
 			al_new1+=coded
+	return ifSame(al_new1)
+
 	
 
 
 
 
 def main():
-	[goodperm(c) for c in [swapaz(), bad(), bad2()]]
-
-	return(None)
+	result=[goodperm(c) for c in [swapaz(), bad(), bad2()]]
+	print(result)
 
 
 main()
