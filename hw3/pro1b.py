583083dbcb4f66d8f09e0d592493edc4eb2df463
ZHIBO WAN <andywan@ZHIBOs-MacBook-Air.local>
2016-08-28 21:28:26 -0400

    done

--- hw3/pro1b.py
+++ hw3/pro1b.py
@@ -1,6 +1,26 @@
 import pickle
 from pro1 import swapaz,bad,bad2
 
-keypath = "./tmp/key.pickle"
+#keypath = "./tmp/key.pickle"
 
-pickle.dump(swapaz(),open(keypath,'wb'))
+
+
+
+
+#pickle.dump(swapaz(),open(keypath,'wb'))
+
+
+
+def saveKey(path,key):
+	if key==None:
+		raise Exception("bad key")
+	else:
+		pickle.dump(key,open(path,'wb'))
+
+
+
+
+#key1=None
+
+#saveKey(keypath,key1)
+
