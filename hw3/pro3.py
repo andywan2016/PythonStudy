0b3d86d498b9821efc4a5f800e828ff73994ed51
andywan2016 <zw2327@columbia.edu>
2016-09-07 14:55:38 +0000

    heheheheh

--- hw3/pro3.py
+++ hw3/pro3.py
@@ -9,7 +9,11 @@ import string
 
 url='https://courseworks.columbia.edu/access/content/group/COMSW3101_002_2015_3/week3/hamlet.html'
 
+
+#initial line and word count
 line_num=0
+word_num=0
+
 tmp_str=" "
 with urllib.request.urlopen(url) as ef:
     for bin in ef:
@@ -17,16 +21,19 @@ with urllib.request.urlopen(url) as ef:
         #print(bin)
         s=bin.decode('utf-8')
         tmp_str=s
+        currentLine_wordCount=len(re.findall('[A-z]+',tmp_str))
+        word_num+=currentLine_wordCount
         #print(s)
-        if line_num==1:
-            break
+        #if line_num==1:
+            #break
         
 ###below are test for regular expression and learning
-splited=tmp_str.split()
-re_result=re.findall("/[A-Z]+/",splited[1])
+#splited=tmp_str.split()
+#re_result=re.findall('[A-z]+',tmp_str)
 
 
-#print(line_num)
-print(tmp_str)
-print(splited)
-print(re_result)
+print(line_num)
+print(word_num)
+#print(tmp_str)
+#print(splited)
+#print(re_result)
